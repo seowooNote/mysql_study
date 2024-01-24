df0a5048f7c310319f566381cb053ec95bf823f0
seowooNote <dkr0210@naver.com>
2024-01-24 17:56:36 +0900

    20240124

---
+++ book_search_query.sql
@@ -0,0 +1,18 @@
+SELECT * FROM db_study.book_tb;
+
+select * from book_tb;
+    
+select
+	bt.book_id,
+    bt.book_name,
+    bt.author_id,
+    at.author_name,
+    bt.publisher_id,
+    pt.publisher_name
+from
+	book_tb bt
+    left outer join author_tb at on(at.author_id = bt.author_id)
+    left outer join publisher_tb pt on(pt.publisher_id = bt.publisher_id)
+where 
+	bt.book_name like '%글%';
+
